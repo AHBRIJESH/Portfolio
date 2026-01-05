0e71519f9b30b7d823daae0022380e25783b121f
AHBRIJESH <ahbrijesh2004@gmail.com>
2026-01-05 11:36:53 +0530

    edit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,13 @@
-
-import React from 'react';
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSection';
-import EducationSection from '@/components/EducationSection';
-import ExperienceSection from '@/components/ExperienceSection';
-import ProjectsSection from '@/components/ProjectsSection';
-import SkillsSection from '@/components/SkillsSection';
-import ContactSection from '@/components/ContactSection';
-import Footer from '@/components/Footer';
+import React from "react";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import EducationSection from "@/components/EducationSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import SkillsSection from "@/components/SkillsSection";
+import ContactSection from "@/components/ContactSection";
+import CredlyBadges from "@/components/CredlyBadges";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
@@ -18,6 +18,7 @@ const Index = () => {
       <ExperienceSection />
       <ProjectsSection />
       <SkillsSection />
+      <CredlyBadges />
       <ContactSection />
       <Footer />
     </div>
